3075e839b17eb0dc225fd8c17f06acbcd06bf0da
tamsay <temitope.longe@decagon.dev>
2021-08-20 14:59:55 +0100

    bug fixes

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://longe-temitope-resume.netlify.app/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
